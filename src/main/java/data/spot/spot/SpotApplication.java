387538e788cd4469788a9c0ccfb1fe6cfa0bf9a2
Alexander Scheibe <35386636+AScheibe@users.noreply.github.com>
2022-09-18 03:57:14 -0500

    fixed maven

--- src/main/java/data/spot/spot/SpotApplication.java
+++ src/main/java/data/spot/spot/SpotApplication.java
@@ -3,11 +3,14 @@ package data.spot.spot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import se.michaelthelin.spotify.SpotifyApi;
+
 @SpringBootApplication
 public class SpotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotApplication.class, args);
+		
 	}
 
 }
